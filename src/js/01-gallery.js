4100596df507a4c8c6927c5f90b0cb7a0a031ecc
VTrofym <v.trof@icloud.com>
2022-09-13 16:59:13 +0300

    added and finished task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,8 +2,43 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+// console.log(galleryContainer)
+const itemsMarkup = createGalleryItems(galleryItems)
+// console.log(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup)
+// console.log(createGalleryItems(galleryItems))
 
-console.dir(galleryItems)
+// 3.
+galleryContainer.addEventListener('click', onGalleryContainerClick)
 
 
+// 1. создание разметки в JS
+function createGalleryItems(galleryItems) {
+  return galleryItems.map(item => {
+    return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${item.preview}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>
+`;
+  }).join('');
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault()
+  if (evt.target.tagName !== 'IMG') {
+    return
+  }
+const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+  instance.show()
+  // console.log(evt.target)
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,82 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeKey = 'videoplayer-current-time';
+
+function durationSaveToStorage({ seconds }) {
+    localStorage.setItem(timeKey, seconds);
+}
+
+window.addEventListener('load', newStart);
+
+player.on('timeupdate', Throttle(durationSaveToStorage, 1000));
+
+function newStart() {
+    if (!localStorage.getItem(timeKey)) {
+        return
+    }
+    const currentVideoTime = localStorage.getItem(timeKey);
+
+    player
+      .setCurrentTime(currentVideoTime)
+    .then(() => {
+    player.play()
+  })
+    .catch (function(error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+          break;
+      }
+    })
+}
+
+
+
+
+
+
+
+// import Player from '@vimeo/player';
+// import Throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// // сделали переменную для ключа
+// const timeKey = 'videoplayer-current-time';
+// // создали функцию, с помощью деструктаризации вытащили секунды собьекта тайм апдейт(записали в локальное хранилище)
+// function durationSaveToStorage({ seconds }) {
+//   localStorage.setItem(timeKey, seconds);
+// }
+// // при перезагрузки страницы перезагружался плеер
+// window.addEventListener('load', newStart);
+
+// player.on('timeupdate', Throttle(durationSaveToStorage, 1000));
+// // функция которая ловит ивент лоад, если в локальном хранилище есть запись, достань их
+// function newStart() {
+//   if (!localStorage.getItem(timeKey)) {
+//     return;
+//   }
+//   const currentVideoTime = localStorage.getItem(timeKey);
+
+//   player
+//     .setCurrentTime(currentVideoTime)
+//     .then(() => {
+//       player.play();
+//       // seconds = the actual time that the player seeked to
+//     })
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           // the time was less than 0 or greater than the video’s duration
+//           break;
+
+//         default:
+//           // some other error occurred
+//           break;
+//       }
+//     });
+// }
